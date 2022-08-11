73b149be7b9792c036cb723e35daa1e3ea4a9bb8
Sajmon Nowak <nowak.sajmon@gmail.com>
2022-08-11 21:49:36 +0200

    removed mantine and related files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,12 @@
-import { MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import myTheme from "./styles/mantineTheme"
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <MantineProvider theme={myTheme} withNormalizeCSS withGlobalStyles>
       <App />
-    </MantineProvider>
   </React.StrictMode>
 );
